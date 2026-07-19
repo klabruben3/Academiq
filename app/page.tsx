b204dc3d20155f2acfdac2aac7ff1f4ee43812b6
klabruben3 <adept.ruben@gmail.com>
2026-07-20 01:58:30 +0200

    Test 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
 import { Academiq, LoadingScreen } from "@/components/layout";
-import { createClient } from "@/lib/supabase/server";
+// import { createClient } from "@/lib/supabase/server";
 
 export default async function Home() {
-  const supabase = await createClient();
+  // const supabase = await createClient();
 
-  const { data: worker, error } = await supabase
-    .from("users")
-    .select("id")
-    .single()
+  // const { data: worker, error } = await supabase
+  //   .from("users")
+  //   .select("id")
+  //   .single()
 
-  if (worker) {
-    console.error(worker);
-  }
-  if (error) {
-    console.log(error);
-  }
+  // if (worker) {
+  //   console.error(worker);
+  // }
+  // if (error) {
+  //   console.log(error);
+  // }
   const isAuth = false;
 
   return <>{isAuth ? <Academiq /> : <LoadingScreen />}</>;
